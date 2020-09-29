13d7a2cd9aa3590b56066a4628f9416b6275126a
Your Name <mazahir.mammadov.std@bhos.edu.az>
2020-09-29 21:25:10 +0400

    Assignment1

--- WEEK2/assignment1.c
+++ WEEK2/assignment1.c
@@ -9,20 +9,21 @@ int a, b;
 a = 50;
 b = 100;
 
-printf("Before changing a=%d, b=%d\n",a,b);
-
 int_swap(&a,&b);
-
-printf("After changing a=%d, b=%d",a,b);
 
 return 0;
 }
 
 void int_swap(int *val1, int *val2){
 
+printf("Before changing: a=%d, b=%d\n",*val1,*val2);
+
 int num;
 
 num = *val1;
 *val1 = *val2;
 *val2 = num;
+
+printf("After changing: a=%d, b=%d",*val1,*val2);
+
 }
