5b538201839db0e73e6d56b071e818a1d72da031
Your Name <mazahir.mammadov.std@bhos.edu.az>
2020-09-29 20:35:47 +0400

    Assignment1

---
+++ WEEK2/assignment1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+void int_swap(int*, int*);
+
+int main(){
+
+int a, b;
+
+a = 50;
+b = 100;
+
+printf("Before changing a=%d, b=%d",a,b);
+
+int_swap(&a,&b);
+
+printf("After changing a=%d, b=%d",a,b);
+
+return 0;
+}
+
+void int_swap(int *val1, int *val2){
+
+int num;
+
+num = *val1;
+*val1 = *val2;
+*val2 = num;
+
+}
