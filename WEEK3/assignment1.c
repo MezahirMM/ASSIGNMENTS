8f02e86e4df08ef43fee705f5c12a6d6d63b4f50
MezahirMM <71690774+MezahirMM@users.noreply.github.com>
2020-10-05 23:24:04 +0400

    Update assignment1.c

--- WEEK3/assignment1.c
+++ WEEK3/assignment1.c
@@ -7,8 +7,7 @@ int main(){
     int a2[] = {0,0,0,0,0};
     int len = sizeof(a1)/sizeof(int);
     int *arr2D[2] = {a1,a2};            // a 2 length array of int pointers, 
-                                        // remember an int pointer pointing to [0] of an array 
-    					// is all you need to point to an array
+                                        
     printf("Before:\n");
     print2DArr(arr2D, 2, len);
     swapArrays(a1, a2, len);
@@ -31,7 +30,6 @@ void swapArrays(int a1[], int a2[], int len){
 }
 void print2DArr(int *arr2D[], int rows, int columns){
     // prints contents of the 2D array in a flattened shape
-    // eg: [[1,2],[3,4]] will be printed as 1 2 3 4\n
     
     for(int i = 0; i < rows; ++i){
         for(int j = 0; j < columns; ++j){
