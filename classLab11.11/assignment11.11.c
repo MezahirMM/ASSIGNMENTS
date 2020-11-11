ecbd949b13a2c33950b851809bf243587cfb4008
Mazahir Mammadov <mazahir.mammadov.std@bhos.edu.az>
2020-11-11 14:15:01 +0400

    pushing lab task

---
+++ classLab11.11/assignment11.11.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+
+void function(char *source, char *integers, char *nonInteger)
+{
+	int a = 0;
+	int b = 0;
+	for(int i = 0; source[i]!='\0';i++)
+	{
+		if((int)source[i]>47 && (int)source[i]<58)
+		{
+			integers[a] = source[i];
+			a++;
+		}
+		else
+		{
+			nonInteger[b] = source[i];
+			b++;
+		}
+	}
+	printf("Integers:%s nonIntegers:%s\n",integers, nonInteger);
+}
+
+int main()
+{
+	char source[] = "abc123def456gh!";
+	char integers[256];
+	char nonIntegers[256];
+
+	function(source, integers, nonIntegers);
+
+}
